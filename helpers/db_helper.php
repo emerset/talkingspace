a6a6a8b46ad716b39ead401828f8b25631f469ab
emerset <emerset@gmail.com>
2015-05-08 16:28:39 -0400

    Create replyCount() (counts # replies to any topic/post)

--- helpers/db_helper.php
+++ helpers/db_helper.php
@@ -1,1 +1,13 @@
 <?php
+/*
+ * Get # of replies per topic
+ */
+function replyCount($topic_id){
+	$db = new Database();
+	$db->query('SELECT * FROM replies WHERE topic_id = :topic_id');
+	$db->bind(':topic_id', $topic_id);
+	// Assign Rows
+	$rows = $db->resultset();
+	// Get Count
+	return $db->rowCount();
+}
