45fb72119c87d53bd372e1e3a11e684e3144eb62
emerset <emerset@gmail.com>
2015-05-08 16:01:31 -0400

    create formatDate() & urlFormat()

--- helpers/format_helper.php
+++ helpers/format_helper.php
@@ -1,1 +1,21 @@
 <?php
+/*
+ * Format Date
+ */
+function formatDate($date) {
+	$date = date("F j, Y, g:i a", strtotime($date));
+	return $date;
+}
+
+/*
+ * URL Format
+ */
+function urlFormat($str) {
+	// Strip out all whitespace
+	$str = preg_replace('/\s*/', $str);
+	// Convert string to lowercase
+	$str = strtolower($str);
+	//URL encode
+	$str = urldecode($str);
+	return $str;
+}
