e3b6659981d3bd0d9a3e315276c7fd9fa95e3738
emerset <emerset@gmail.com>
2015-05-08 17:29:26 -0400

    Create getCategories()

--- helpers/db_helper.php
+++ helpers/db_helper.php
@@ -7,7 +7,17 @@ function replyCount($topic_id){
 	$db->query('SELECT * FROM replies WHERE topic_id = :topic_id');
 	$db->bind(':topic_id', $topic_id);
 	// Assign Rows
-	$rows = $db->resultset();
+	$db->resultset();
 	// Get Count
 	return $db->rowCount();
+}
+
+/*
+ * Get Categories
+ */
+function getCategories(){
+	$db = new Database();
+	$db->query('SELECT * FROM `categories`');
+	$results = $db->resultset();
+	return $results;
 }
