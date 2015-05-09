ac24a6d988620aac49238485a73e721a86aaccee
emerset <emerset@gmail.com>
2015-05-09 11:36:13 -0400

    If $_GET['user'] is set, assign getByUser() a $template variable

--- topics.php
+++ topics.php
@@ -6,6 +6,9 @@ $topic = new Topic();
 // Get category from URL
 $category = isset($_GET['category']) ? $_GET['category'] : null;
 
+// Get user from URL
+$user_id = isset($_GET['user']) ? $_GET['user'] : null;
+
 // Get template and assign vars
 $template = new Template('templates/topics.php');
 
@@ -13,10 +16,20 @@ $template = new Template('templates/topics.php');
 $template->totalTopics = $topic->getTotalTopics();
 $template->totalCategories = $topic->getTotalCategories();
 $template->totalUsers = $topic->getTotalUsers();
+
+// Check for category filter
 if(isset($category)){
 	$template->topics = $topic->getByCategory($category);
 	$template->title = 'Posts In "'.$topic->getByCategory($category)->name.'"';
-} else {
+}
+	
+// Check for user filter
+if(isset($user_id)){
+	$template->topics = $topic->getByUser($user_id);
+	$template->title = 'Posts By "'.$topic->getByUser($user_id)->name.'"';
+}
+
+if(!isset($category) && !isset($user_id)){
 	$template->topics = $topic->getAllTopics();
 }
 
