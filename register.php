7e5e37d43950d932fb356fb7c8225828ea701955
emerset <emerset@gmail.com>
2015-05-09 16:03:44 -0400

    Call new Topic() obj, create data[] array if form was submitted

--- register.php
+++ register.php
@@ -1,6 +1,31 @@
 <?php require('core/init.php'); ?>
 
-<?php 
+<?php
+// Create Topic Object
+$topic = new Topic();
+
+// Create User Object
+$user = new User();
+
+if (isset($_POST['register'])) {
+	// Create Data Array
+	$data = array();
+	$data['name'] = $_POST['name'];
+	$data['email'] = $_POST['email'];
+	$data['username'] = $_POST['username'];
+	$data['password'] = md5($_POST['password']);
+	$data['password2'] = md5($_POST['password2']);
+	$data['about'] = $_POST['about'];
+	$data['last_activity'] = date("Y-m-d H:i:s");
+	
+	// Upload Avatar image
+	if ($user->uploadAvatar()){
+		$data['avatar'] = $_FILES['avatar']['name'];
+	} else {
+		$data['avatar'] = 'noimage.png';
+	}
+}
+
 // Get template and assign vars
 $template = new Template('templates/register.php');
 
