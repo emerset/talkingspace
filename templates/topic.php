aea223578cd7e023d1c4faff12d0ce153d6b988c
emerset <emerset@gmail.com>
2015-05-09 10:56:05 -0400

    Make replies dynamic in topic.php

--- templates/topic.php
+++ templates/topic.php
@@ -19,49 +19,27 @@
 			</div>
 		</div>
 	</li>
+	<?php foreach ($replies as $reply) : ?>
 	<li class="topic topic">
 		<div class="row">
 			<div class="col-md-2">
 				<div class="user-info">
-					<img class="avatar pull-left" src="img/gravatar.jpg" />
+					<img class="avatar pull-left" src="<?php echo BASE_URI ?>/images/avatars/<?php echo $reply->avatar ?>" />
 					<ul>
-						<li><strong>Casey</strong></li>
-						<li>43 Posts</li>
-						<li><a href="profile.php">Profile</a></li>
+						<li><strong><?php echo $reply->username ?></strong></li>
+						<li><?php echo userPostCount($reply->user_id) ?> Posts</li>
+						<li><a href="<?php echo BASE_URI ?>/topics.php?user=<?php echo $reply->user_id ?>">Profile</a></li>
 					</ul>
 				</div>
 			</div>
 			<div class="col-md-10">
 				<div class="topic-content pull-right">
-					<p>Congrats on how to make a href and inserting an image...
-
-					You can learn HTML/CSS pretty fast, though how to use it in different scenarios is a whole other deal.
-
-					I like to check out tutorials on how to implement the newest within html/css (html5 / css3), </p>
-
+					<?php echo $reply->body ?>
 				</div>
 			</div>
 		</div>
 	</li>
-	<li class="topic topic">
-		<div class="row">
-			<div class="col-md-2">
-				<div class="user-info">
-					<img class="avatar pull-left" src="img/gravatar.jpg" />
-					<ul>
-						<li><strong>JohnD</strong></li>
-						<li>43 Posts</li>
-						<li><a href="profile.php">Profile</a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md-10">
-				<div class="topic-content pull-right">
-					<p>w3schools is very good. I can't code an entire site, but I can handle basic things lik links, fonts and colors. I'm not intimidated by the site of code.</p>
-				</div>
-			</div>
-		</div>
-	</li>
+	<?php endforeach; ?>
 </ul>
 <h3>Reply To Topic</h3>
 <form role="form">
