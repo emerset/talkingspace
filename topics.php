2bc74b3b41d7bdaf4853d1405685294380b2bfb3
emerset <emerset@gmail.com>
2015-05-08 22:16:42 -0400

    Assign template variables for BASE_URL/topics.php

--- topics.php
+++ topics.php
@@ -1,10 +1,24 @@
 <?php require 'core/init.php'; ?>
 <?php 
+// Create topics object
+$topic = new Topic();
+
+// Get category from URL
+$category = isset($_GET['category']) ? $_GET['category'] : null;
+
 // Get template and assign vars
 $template = new Template('templates/topics.php');
 
 //Assign vars
-
+$template->totalTopics = $topic->getTotalTopics();
+$template->totalCategories = $topic->getTotalCategories();
+$template->totalUsers = $topic->getTotalUsers();
+if(isset($category)){
+	$template->topics = $topic->getByCategory($category);
+	$template->title = 'Posts In "'.$topic->getByCategory($category)->name.'"';
+} else {
+	$template->topics = $topic->getAllTopics();
+}
 
 // Display template
 echo $template;
